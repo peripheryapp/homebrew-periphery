565f2a51a8f3f1c253e85b70f7c5e5fe9aa2431d
Ian Leitch <port001@gmail.com>
2018-08-06 12:13:15 +0100

    0.11.1

--- Casks/periphery.rb
+++ Casks/periphery.rb
@@ -1,6 +1,6 @@
 cask 'periphery' do
-  version '0.11.0'
-  sha256 '4b72b6e730d11b1be519c5c42d8f5d498cd34cd2e39995da8e891513de35f401'
+  version '0.11.1'
+  sha256 'b124315f7e51b75e98b664f7886f6d4656642a3aa71840dbf54db86cfbbbe427'
   url "https://github.com/peripheryapp/releases/releases/download/#{version}/periphery-v#{version}.zip"
   name 'Periphery'
   homepage 'https://peripheryapp.com'
