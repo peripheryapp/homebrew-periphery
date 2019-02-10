e61206ff170ac41bf5758301f1b778194a0bc696
Ian Leitch <port001@gmail.com>
2019-02-10 18:39:34 +0100

    Update release path.

--- Casks/periphery.rb
+++ Casks/periphery.rb
@@ -1,9 +1,9 @@
 cask 'periphery' do
   version '1.3.0'
   sha256 'eb18a85f1fc085d039131bcd586061a83e20457feafdb4512326f8a5ed6d6df5'
-  url "https://github.com/peripheryapp/releases/releases/download/#{version}/periphery-v#{version}.zip"
+  url "https://github.com/peripheryapp/periphery/releases/download/#{version}/periphery-v#{version}.zip"
   name 'Periphery'
-  homepage 'https://peripheryapp.com'
+  homepage 'https://github.com/peripheryapp/periphery'
   binary 'periphery'
   depends_on macos: '>= :yosemite'
 
