8f5c24c49a64f2c37aac9f01fa198876695d6fa5
Ian Leitch <port001@gmail.com>
2019-02-19 21:19:51 +0100

    Use 1.3.0 sha256

--- Casks/periphery.rb
+++ Casks/periphery.rb
@@ -1,6 +1,6 @@
 cask 'periphery' do
   version '1.3.0'
-  sha256 'eb18a85f1fc085d039131bcd586061a83e20457feafdb4512326f8a5ed6d6df5'
+  sha256 'a10e4b01f280bbc8175720ce35131e034e34084dd2d81e0c9240fc0d35887f5e'
   url "https://github.com/peripheryapp/periphery/releases/download/#{version}/periphery-v#{version}.zip"
   name 'Periphery'
   homepage 'https://github.com/peripheryapp/periphery'
