2ded86b109ebeb210863ea50694d9aa984bb9c90
Ian Leitch <ian@leitch.io>
2022-11-20 19:12:19 +0100

    2.10.1

--- Casks/periphery.rb
+++ Casks/periphery.rb
@@ -1,6 +1,6 @@
 cask 'periphery' do
-  version '2.10.0'
-  sha256 'f1736ef22c603c76073ca7758e6adc9f507c0f1a5f26ff33811ab85840595b21'
+  version '2.10.1'
+  sha256 '543e1a1e88fd1a3601977ff60ab05bd0ad760585aa642da422e44e6f0e295d14'
   url "https://github.com/peripheryapp/periphery/releases/download/#{version}/periphery-v#{version}.zip"
   name 'Periphery'
   homepage 'https://github.com/peripheryapp/periphery'
