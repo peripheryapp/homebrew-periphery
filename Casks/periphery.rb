b99261b7cfd8873fd998c0b59723f7d0528ac960
ileitch <ian@leitch.io>
2023-01-02 17:12:17 +0000

    Fix hash.

--- Casks/periphery.rb
+++ Casks/periphery.rb
@@ -1,6 +1,6 @@
 cask 'periphery' do
   version '2.10.3'
-  sha256 ''
+  sha256 '6843295516e136421e17e91d784dfbf10f0d886651daa11d909635ed4fccaf2d'
   url "https://github.com/peripheryapp/periphery/releases/download/#{version}/periphery-v#{version}.zip"
   name 'Periphery'
   homepage 'https://github.com/peripheryapp/periphery'
