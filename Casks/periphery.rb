743c8aa7e749a1778d121b2258874f0aa41dc3ee
Ian Leitch <ian@leitch.io>
2024-05-20 20:35:00 +0100

    Fix 2.19.0 sha256

--- Casks/periphery.rb
+++ Casks/periphery.rb
@@ -1,6 +1,6 @@
 cask 'periphery' do
   version '2.19.0'
-  sha256 '0b9a8ced53c6aadcfd61849a3823ce689de81682d75bc8627abb53f1478853a4'
+  sha256 'b4b6ccae7e6302388c5670cb3830e5952c4580b8109d42271271e7982156d9b9'
   url "https://github.com/peripheryapp/periphery/releases/download/#{version}/periphery-#{version}.zip"
   name 'Periphery'
   homepage 'https://github.com/peripheryapp/periphery'
